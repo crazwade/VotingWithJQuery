2b3f47df1758fd696fb7b0ed7be16d298949fff5
crazwade <31063679+crazwade@users.noreply.github.com>
2018-07-07 20:15:27 +0800

    Add files via upload

--- js/all.js
+++ js/all.js
@@ -1,5 +1,427 @@
+var order = {};
+
 $( document ).ready(function() {
-    $('.nav2 h4').click(function(event){
-        $('.login').slideToggle(500);
+    $('#login_button').click(function(event){
+        $('.login').slideToggle(300);
     })
+    InitialUI();
 });
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+function HttpGet(name) {
+    if (name = (new RegExp('[?&]' + encodeURIComponent(name) + '=([^&]*)')).exec(location.search))
+        return decodeURIComponent(name[1]);
+}
+
+function ShowIndexVoteInfo()
+{
+    var voteItemStr = '<div class="nowvoting"> <h2>台北市市長選舉</h2> <div class="candidate"> <ul> <li><img class="candidate_image" src="images/logo1.png" alt=""></li> <li><img class="candidate_image" src="images/logo1.png" alt=""></li> <li><img class="candidate_image" src="images/logo1.png" alt=""></li> <li><img class="candidate_image" src="images/logo1.png" alt=""></li> <div class="clear"></div> </ul> </div> </br> <button class="joinvote">投票詳情</button> </div>'
+    var voteListArea = document.querySelector(".allvote");
+    var str = "";
+    database.ref('voting/Vote').once('value', function (snapshot) {
+        snapshot.forEach(function (data) {
+            str = "";
+            str += '<div class="nowvoting"> <h2>'+data.val().VoteName+'</h2> <div class="candidate"> <ul>';
+            for(index in data.val().Candidate)
+            {
+                str += '<li><img class="candidate_image" src="images/candidates/' + data.val().Candidate[index].Image +'" alt=""></li>';
+            }
+            str += '<div class="clear"></div> </ul> </div> </br> <a href="vote.html?votename=' + data.val().VoteName+'" class="joinvote">投票詳情</a> </div>';
+            voteListArea.innerHTML += str;
+        });
+        document.querySelector("#loading").outerHTML = "";
+    }).then(function () {
+    });
+}
+
+function ShowVotePage()
+{
+    var votename = HttpGet("votename");
+    if (votename == "")
+    {
+        alert("指定的投票不存在");
+        location.href = 'index.html';
+        return;
+    }
+    
+    database.ref('voting/Vote/').once('value', function (snapshot) {
+        var voteListArea = document.querySelector(".nowvoting");
+        var canVote = false;
+        snapshot.forEach(function (data) {
+            if (data.val().VoteName == votename)
+            {
+                var str;
+                str = "";
+                str += '<h2 id = "vote_name">' + data.val().VoteName + '</h2>'
+                for (index in data.val().Candidate) {
+                    order[index] = data.val().Candidate[index].Name;
+                    str += '<div class="acandidate" id="' + data.val().Candidate[index].Name + '"> <img class="candidate-image" src="images/candidates/' + data.val().Candidate[index].Image + '" alt=""> <h4 class="candidate-name">' + data.val().Candidate[index].Name + '</h4>';
+                    // if (canVote)
+                    //     str += ' <input type="radio" name="cadidate" id="check' + index + '" value="' + data.val().Candidate[index].Name + '" class="cb"> <label for="check' + index + '"></label>';
+                    // else
+                    str += ' <input type="radio" name="cadidate" id="check' + index + '" value="' + data.val().Candidate[index].Name + '" class="cb"> <label style="opacity:0;" for="check' + index + '"></label>';
+                    str += '<div class="info"> <div class="progress-bar-bg"> <div class="progress-bar"></div><p class="num">' + 0 + '</p></div> <p class="percentage"></p></div> <div class="clear"></div> </div>';
+                }
+                // if (canVote)
+                //     str += '<button id="vote-button" class="button" onclick="OnVoteButtonClicked()">確認</button>';
+                // else
+                    str += '<button style="opacity:0;" id="vote-button" class="button" onclick="OnVoteButtonClicked()">確認</button>';
+                voteListArea.innerHTML = str;
+            }
+        });
+    }).then(function () {
+        UpdateNum(votename);
+        CheckCanVote();
+    });
+}
+
+function CheckCanVote()
+{
+    var email = "";
+    var votename = HttpGet("votename");
+    if (firebase.auth().currentUser)
+    {
+        email = firebase.auth().currentUser.email;
+        if (document.getElementsByClassName("hint").length > 0)
+            document.getElementsByClassName("hint")[0].outerHTML = "";
+    }
+    else
+    {
+        if (document.getElementsByClassName("hint").length <= 0) {
+            var hint = '<p class="hint" style=" color: red; font-weight: bold; "><a href=javascript:$(".login").slideDown()>登入</a>或<a href="signup.html">註冊</a>以參與投票</p>';
+            document.querySelector(".nowvoting").innerHTML += hint;
+        }
+    }
+    database.ref('member').once('value', function (snapshot) {
+        snapshot.forEach(function (data) {
+            if (data.val().Email.toUpperCase() == email.toUpperCase()) {
+                if (votename.substring(0, 3) == data.val().Area) {
+                    database.ref('voting/VoteRecord').once('value', function (snapshot) {
+                        var flag = false;
+                        snapshot.forEach(function (data) {
+                            if(data.val().Voter == email && data.val().VoteName == votename)
+                            {
+                                flag = true;
+                            }
+                        });
+                        if(!flag)
+                        {
+                            canVote = true;
+                            document.querySelector("#vote-button").style.opacity = 1;
+                            var radios = document.querySelectorAll(".acandidate label");
+                            for(var i = 0; i < radios.length; i++)
+                            {
+                                radios[i].style.opacity = "1";
+                            }                            
+                        }
+                        else
+                        {
+                            if (document.getElementsByClassName("hint").length <= 0) {
+                                var hint = '<p class="hint" style=" color: red; font-weight: bold; ">你已經參與過此次投票</p>';
+                                document.querySelector(".nowvoting").innerHTML += hint;
+                            }
+                        }
+                    });                    
+                }
+                else if (document.getElementsByClassName("hint").length <= 0)
+                {
+                    var hint = '<p class="hint" style=" color: red; font-weight: bold; ">你只能參與你所在縣市的投票</p>';
+                    document.querySelector(".nowvoting").innerHTML += hint;
+                }
+            }
+        })
+    });
+}
+
+function DoOrder()
+{
+    var votename = HttpGet("votename");
+    var flag = true;
+    var VoteData = GetScoreByVoteName(votename);
+    console.log(VoteData);
+    while (flag) {
+        flag = false;
+        for (index in order) {
+            if (VoteData[order[index]] == undefined)
+                VoteData[order[index]] = 0;
+            if(index == 0)
+                continue;
+            if (VoteData[order[index]] > VoteData[order[index-1]])
+            {
+                Swap(index, index-1);
+                flag = true;
+            }
+        }
+    }
+    var i = 0;
+    console.log(VoteData);
+    for (index in order) {
+        if (i == 0 && VoteData[order[index]] != 0)
+            document.getElementById(order[index]).style.background = "#ce3c3c";
+        else
+            document.getElementById(order[index]).style.background = "unset";
+        i++;
+    }
+}
+
+function GetScoreByVoteName(VoteName)
+{
+    var VoteData = {};
+    database.ref('voting/VoteRecord/').once('value', function (snapshot) {
+        snapshot.forEach(function (data) {
+            if (data.val().VoteName == VoteName) {
+                if (VoteData[data.val().VoteTo] == undefined)
+                    VoteData[data.val().VoteTo] = 1;
+                else
+                    VoteData[data.val().VoteTo]++;
+            }
+        });
+    });
+    return VoteData;
+}
+
+async function UpdateNum(VoteName)
+{
+    var VoteData = {};
+    await database.ref('voting/VoteRecord/').once('value', function (snapshot) {
+        snapshot.forEach(function (data) {
+            if (data.val().VoteName == VoteName) {
+                if (VoteData[data.val().VoteTo] == undefined)
+                    VoteData[data.val().VoteTo] = 1;
+                else
+                    VoteData[data.val().VoteTo]++;
+            }
+        });
+    });
+    var data = VoteData;
+    var allVoteCount = 0;
+    for(index in data)
+    {
+        allVoteCount += Number.parseInt(data[index]);
+    }
+    var candidates = document.querySelectorAll(".acandidate");
+    for(var i = 0; i < candidates.length; i++)
+    {
+        var id = candidates[i].id;
+        var name = candidates[i].querySelector(".candidate-name").innerText;
+        var num = candidates[i].querySelector(".num");
+        var percentage = candidates[i].querySelector(".percentage");
+        var progressbar = candidates[i].querySelector(".progress-bar");
+        if (data[name] == undefined)
+            data[name] = 0;
+        num.innerText = data[name];
+        var percentageValue;
+        if (allVoteCount != 0)
+            percentageValue = (Number.parseFloat(data[name]) / allVoteCount) * 100;
+        else
+            percentageValue = 0;
+        var percentageFixed = new Number(percentageValue);
+        percentageFixed = percentageFixed.toFixed(1);
+        percentage.innerText = percentageFixed+ "%";
+        progressbar.style.width = percentageFixed+"%";
+    }
+    DoOrder();
+}
+var isInitialize = false;
+function Swap(index1, index2)
+{
+    var tmp = order[index1];
+    order[index1] = order[index2];
+    order[index2] = tmp;
+    var id1 = "#"+order[index1];
+    var id2 = order[index2];
+    if (!isInitialize)
+        SwapAnim(id1, id2);
+    else
+        SwapAnim(id1, id2);
+    isInitialize = true;
+}
+
+var isSwaping = false;
+function FastSwapAnim(id1, id2)
+{
+    if (isSwaping)
+        window.setTimeout(function () {
+            FastSwapAnim(id1, id2);
+        }, 100);
+    else
+        isSwaping = true;
+    $(id1).swap({
+        target: id2, // Mandatory. The ID of the element we want to Swap with  
+        speed: 0, // Optional. The time taken in milliseconds for the animation to occur  
+        callback: function () { // Optional. Callback function once the Swap is complete  
+            // alert("Swap Complete");
+            var element1 = document.querySelector(id1);
+            var element2 = document.querySelector("#" + id2);
+            element1.style = "background:" + element1.style.background + ";";
+            element2.style = "background:" + element2.style.background + ";";
+            var tmp = element1.outerHTML;
+            element1.outerHTML = element2.outerHTML;
+            element2.outerHTML = tmp;
+            isSwaping = false;
+        }
+    });
+}
+
+function SwapAnim(id1, id2)
+{
+    if (isSwaping)
+        window.setTimeout(function(){
+            SwapAnim(id1, id2);
+        }, 100);
+    else
+        isSwaping = true;
+    $(id1).swap({
+        target: id2, // Mandatory. The ID of the element we want to Swap with  
+        speed: 300, // Optional. The time taken in milliseconds for the animation to occur  
+        callback: function () { // Optional. Callback function once the Swap is complete  
+            // alert("Swap Complete");
+            var element1 = document.querySelector(id1);
+            var element2 = document.querySelector("#"+id2);
+            element1.style = "background:"+element1.style.background+";";
+            element2.style = "background:" + element2.style.background+";";
+            var tmp = element1.outerHTML;
+            element1.outerHTML = element2.outerHTML;
+            element2.outerHTML = tmp;
+            isSwaping = false;
+        }
+    });
+}
+
+function OnVoteButtonClicked()
+{
+    var target = GetSelectedCandidator();
+    if (target != undefined)
+        CheckVoteDialogBox(target);
+}
+
+function CheckVoteDialogBox(Name) {
+    if (confirm("確定要投給「"+Name+"」嗎？")) {
+        var votename = HttpGet("votename");
+        Vote(votename, Name, GetUser());
+        DisableVoting();
+    } else {
+        txt = "You pressed Cancel!";
+    }
+}
+
+function GetUser()
+{
+    return firebase.auth().currentUser.email;
+}
+
+function DisplayUserData()
+{
+    if(isLogin())
+    {
+        var email = firebase.auth().currentUser.email;
+
+        var newName = document.getElementById("name");
+        var newEmail = document.getElementById("email");
+        newEmail.value = email;
+        database.ref('member').once('value', function (snapshot) {
+            snapshot.forEach(function (data) {
+                if (data.val().Email.toUpperCase() === email.toUpperCase()) {
+                    newName.value = data.val().Name;
+
+                    ShowLoadingPanel(false);
+                }
+            })
+        });
+    }
+    else
+    {
+        window.setTimeout(DisplayUserData, 100);
+        ShowLoadingPanel(true);
+    }
+}
+
+function GetSelectedCandidator()
+{
+    var radios = document.getElementsByName('cadidate');
+
+    for (var i = 0, length = radios.length; i < length; i++) {
+        if (radios[i].checked) {
+            return radios[i].value;
+        }
+    }
+}
+
+function DisableVoting()
+{
+    document.querySelector("#vote-button").style.opacity = 0;
+    var radios = document.querySelectorAll("label");
+    for(index in radios)
+    {
+        radios[index].style.opacity = 0;
+    }
+}
+
+function ShowFullScreenMessage(message)
+{
+    var panel = document.querySelector(".fullscreen");
+    var text = document.querySelector(".fullscreen #popup-txt");
+    text.innerText = message;
+    panel.style.display = "block";
+}
+
+function DisableFullScreenMessage()
+{
+    var panel = document.querySelector(".fullscreen");
+    panel.style.display = "none";
+}
+
+function Reload()
+{
+    window.location.reload(false); 
+}
+
+function ShowLoadingPanel(isShow)
+{
+    if(isShow)
+        document.getElementById("loadingpanel").style.display = "block";
+    else
+        document.getElementById("loadingpanel").style.display = "none";
+}
+
+function ShowVoteRecord()
+{
+    console.log("dododdo");
+    if(!isLogin())
+    {
+        window.setTimeout(function(){
+            ShowVoteRecord();
+        }, 100);
+    }
+    else
+    {
+        var email = firebase.auth().currentUser.email;
+        var str = "";
+        var record = document.getElementById("record");
+        console.log(email);
+        database.ref('voting/VoteRecord').once('value', function (snapshot) {
+            snapshot.forEach(function (data) {
+                if (email.toUpperCase() == data.val().Voter.toUpperCase()) {
+                    str += '<tr> <td>' + data.val().VoteName + '</td> <td>' + data.val().VoteTime + '</td> </tr>';
+                }
+            });
+            record.innerHTML += str;
+        });
+    }
+    
+}
--- js/database.js
+++ js/database.js
@@ -10,23 +10,174 @@ firebase.initializeApp(config);
 
 var database = firebase.database();
 
-function SignUp(email, password)
+function OnSignUpButtonClicked()
 {
+    var email = document.querySelector("#signup-email").value;
+    var name = document.querySelector("#signup-name").value;
+    var area = document.querySelector("#signup-area").value;
+    var password = document.querySelector("#signup-password").value;
+    SignUp(email,password, name, area);
+}
+
+function SignUp(email, password, name, area)
+{
+    var isSuccess = true;
+    ShowFullScreenMessage("註冊中...");
     firebase.auth().createUserWithEmailAndPassword(email, password)
         .catch(function (error) {
             // Handle Errors here.
             var errorCode = error.code;
             var errorMessage = error.message;
+            isSuccess = false;
             if (errorCode == 'auth/weak-password') {
-                alert('The password is too weak.');
+                alert('密碼強度太弱，請重新設定');
             } else {
                 alert(errorMessage);
             }
             console.log(error);
+        }).then(function(){
+            if(isSuccess)
+            {
+                ShowFullScreenMessage("註冊成功");
+                AddUserToDatabase(email, name, area);
+                window.setTimeout(redirectToIndex,1000);
+            }
+            else
+                DisableFullScreenMessage();
         });
 }
 
-function Vote(VoteName, CandidateID, Voter) {
+function LogOut()
+{
+    firebase.auth().signOut().then(function () {
+        window.location.href = "index.html";
+    }, function (error) {
+        // An error happened.
+    });
+}
+
+function AddUserToDatabase(email, name, area)
+{
+    database.ref('member').push({
+        Email: email,
+        Name: name,
+        Area: area
+    }).then(function(){
+        // if(isLogin())
+        //     window.location.reload(false);
+    });
+}
+
+function OnLogInButtonClicked()
+{
+    var email = document.querySelector("#login_email").value;
+    var password = document.querySelector("#login_password").value;
+    ShowFullScreenMessage("登入中...");
+    LogIn(email,password);
+}
+
+function LogIn(email, password)
+{
+    firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION)
+        .then(function () {
+            // Existing and future Auth states are now persisted in the current
+            // session only. Closing the window would clear any existing state even
+            // if a user forgets to sign out.
+            // ...
+            // New sign-in will be persisted with session persistence.
+            return firebase.auth().signInWithEmailAndPassword(email, password)
+                .then(function (user) {
+                    FinishLogin();
+                })
+                .catch(function (error) {
+                    DisableFullScreenMessage();
+                    console.log(error);
+                    alert("登入失敗");
+                });
+        })
+        .catch(function (error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+        });
+
+}
+
+function isLogin()
+{
+    var user = firebase.auth().currentUser;
+    if (user != null) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function redirectToIndex()
+{
+    console.log("redir");
+    if(isLogin())
+    {
+        window.location.href = "index.html";
+        console.log("redirrrr");
+    }
+    else
+    {
+        window.setInterval(function () {
+            redirectToIndex();
+        }, 100);
+    }
+}
+
+function FinishLogin()
+{    
+    DisableFullScreenMessage();
+    $('.login').slideToggle(300);    
+    InitialUI();
+}
+
+firebase.auth().onAuthStateChanged(function (user) {
+    console.log("changeState");
+    if (user) {
+        document.querySelector("#user-button").style.display = "block";
+        document.querySelector("#login_button").style.display = "none";
+    } else {
+        document.querySelector("#user-button").style.display = "none";
+        document.querySelector("#login_button").style.display = "block";
+    }
+});
+
+function InitialUI()
+{
+    if (isLogin()) {
+        document.querySelector("#user-button").style.display = "block";
+        document.querySelector("#login_button").style.display = "none";
+        console.log("login");
+    }
+    else {
+        document.querySelector("#user-button").style.display = "none";
+        document.querySelector("#login_button").style.display = "block";
+        console.log(" not login");
+    }
+}
+
+function OnForgetPasswordButtonClicked()
+{
+    var email = document.getElementById("forget_email").value;
+    SendForgetPasswordEmail(email);
+    ShowLoadingPanel(true);
+}
+
+function SendForgetPasswordEmail(email) {
+    firebase.auth().sendPasswordResetEmail(email).then(function () {
+        ShowLoadingPanel(false);
+        alert("已將重設密碼郵件寄到信箱");
+    }).catch(function (error) {
+        // An error happened.
+    });
+}
+
+function Vote(VoteName, CandidateName, Voter) {
     database.ref('voting/VoteRecord').once('value', function (snapshot) {
         snapshot.forEach(function (data) {
             if(data.val().VoteName == VoteName && data.val().Voter == Voter)
@@ -38,9 +189,119 @@ function Vote(VoteName, CandidateID, Voter) {
     database.ref('voting/VoteRecord').push({
         VoteName: VoteName,
         VoteTime: GetCurrentDate(),
-        VoteTo: CandidateID,
-        Voter: "UserEmail"
+        VoteTo: CandidateName,
+        Voter: Voter
+    }).then(function(){
+        console.log("投票成功");
+        UpdateNum(HttpGet("votename"));
     });
+}
+
+function GetUserArea()
+{
+    var email = firebase.auth().currentUser.email;
+    database.ref('member').once('value', function (snapshot) {
+        snapshot.forEach(function (data) {
+            if(data.val().Email === email)
+            {
+                Area = data.val().Area;
+                return Area;
+            }
+        })
+    });
+}
+
+function OnUpdateUserDataButtonClicked()
+{
+    var newName = document.getElementById("name").value;
+    var oldPassword = document.getElementById("password").value;
+    var newPassword = document.getElementById("newpassword").value;
+    var newPassword2 = document.getElementById("newpassword2").value;
+    var newEmail = document.getElementById("email").value;
+    if(newPassword != newPassword2)
+    {
+        alert("兩次密碼輸入不同");
+    }
+    else
+    {
+        UpdateUserData(newName, oldPassword, newPassword, newEmail);
+    }
+}
+
+function UpdateUserData(newName, oldPassword, newPassword, newEmail){
+    var isFail = false;
+    console.log(oldPassword);
+    firebase.auth().currentUser.reauthenticateWithCredential(firebase.auth.EmailAuthProvider.credential(firebase.auth().currentUser.email, oldPassword)).catch(function(e){
+        alert("密碼錯誤");
+        isFail = true;
+    }).then(async function(){
+        if(!isFail)
+        {
+            ShowLoadingPanel(true);
+            var nowEmail = firebase.auth().currentUser.email;
+            var isError = false;
+            await database.ref('member').once('value', function (snapshot) {
+                snapshot.forEach(function (data) {
+                    if (data.val().Email.toUpperCase() === nowEmail.toUpperCase()) {
+                        database.ref('member').child('/'+data.key)
+                            .update({
+                                Name: newName,
+                                Email: newEmail
+                            }).catch(function (error) {
+                                isError = true;
+                            });
+                    }
+                });
+            });
+            if (isFail) {
+                alert("發生錯誤，請重新輸入");
+                return;
+            }
+            var user = firebase.auth().currentUser;
+
+            console.log("change password");
+            await user.updatePassword(newPassword).then(function () {
+                // Update successful.
+            }).catch(function (error) {
+                isError = true;
+                });
+            if (isFail)
+            {
+                alert("發生錯誤，請重新輸入");
+                return;
+            }
+            await user.updateEmail(newEmail).then(function () {
+                // Update successful.
+            }).catch(function (error) {
+                isError = true;
+                });
+            if (isFail) {
+                alert("發生錯誤，請重新輸入");
+                return;
+            }
+            await database.ref('voting/VoteRecord').once('value', function (snapshot) {
+                snapshot.forEach(function (data) {
+                    if (data.val().Voter.toUpperCase() === nowEmail.toUpperCase()) {
+                        database.ref('voting/VoteRecord').child('/' + data.key)
+                            .update({
+                                Voter: newEmail
+                            }).catch(function (error) {
+                                isError = true;
+                            });
+                    }
+                });
+            });
+            if(isError)
+                alert("發生錯誤，請重新輸入");
+            else
+            {
+                ShowLoadingPanel(false);
+                alert("更新成功");
+                window.location.href = 'index.html';
+            }
+        }
+    })
+    
 }
 
 function GetCurrentDate()
@@ -64,10 +325,7 @@ function GetCurrentDate()
 
 database.ref('voting').on('value', function (snapshot) {
     snapshot.forEach(function (data) {
-        console.log(data.val());
-        // str += '<li>' + data.val().name + ":  " + data.val().message + '</li>'
-        // talkArea.innerHTML = str;
+        UpdateNum(HttpGet("votename"));
     })
 });
-
-Vote("高雄市市長選舉", 1);
+// Vote("高雄市市長選舉", 1);
